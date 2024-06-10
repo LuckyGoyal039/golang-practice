af3fb5dbc3d80fcae6bf8e2ae19b31ae420b6cfc
Lucky Goyal <luckygoyal1@districtd.in>
2024-06-10 11:32:31 +0530

    decoding json

--- jsonData/main.go
+++ jsonData/main.go
@@ -17,7 +17,9 @@ func main() {
 
 	fmt.Println("Play with Json data")
 
-	EncodingJson()
+	// EncodingJson()
+
+	DecodingJson()
 }
 
 func EncodingJson() {
@@ -36,4 +38,38 @@ func EncodingJson() {
 	}
 
 	fmt.Printf("%s\n", finalJson)
+}
+
+func DecodingJson() {
+	jsonData := []byte(`
+	 {
+                "Name": "react tutorials",
+                "Price": 399,
+                "Platform": "learnonline.com",
+                "tags": [
+                        "golang",
+                        "frontend"
+                ]
+        }`)
+
+	var lcoCourse course
+	checkValid := json.Valid(jsonData)
+
+	if checkValid {
+		fmt.Println("JSON is valid")
+		json.Unmarshal(jsonData, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("JSON is not valid")
+	}
+
+	// just want to addd data in key value pair
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonData, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("Key is %v and value is %v and Type is: %T\n", k, v, v)
+	}
 }
